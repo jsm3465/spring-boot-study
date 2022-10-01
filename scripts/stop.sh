e1e0755178079fa079591d3ffd1f4e712651ffd4
sangmin.ju <sangmin.ju@navercorp.com>
2022-10-01 18:02:27 +0900

    자동 배포 설정 수정2

--- scripts/stop.sh
+++ scripts/stop.sh
@@ -11,7 +11,9 @@ IDLE_PID=$(lsof -ti tcp:{IDLE_PORT})
 
 if [ -z ${IDLE_PID} ]
 then
-    echo "> kill -15 $IDLE_PID"
-    kill -15 ${IDLE_PID}
-    sleep 5
+  echo "> $IDLE_PORT 에서 구동 중인 애플리케이션 pid 확인"
+else
+  echo "> kill -15 $IDLE_PID"
+  kill -15 ${IDLE_PID}
+  sleep 5
 fi
